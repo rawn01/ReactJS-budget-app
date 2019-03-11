20d679b7d1f3a92183b917bd27aafd376a198f2d
unknown <rohan.kaos@gmail.com>
2019-03-11 17:10:45 +0530

    Setup production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,49 @@
 const path = require('path');
+const MiniExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-   entry: './src/app.js',
-   output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
-   },
-   mode: 'development',
-   module: {
-      rules: [{
-         loader: 'babel-loader',
-         test: /\.js$/,
-         exclude: /node_modules/
+module.exports = (env) => {
+   const isProduction = env === 'production';
+   const CSSExtract = new MiniExtractPlugin({ filename: 'styles.css' });
+
+   return {
+      entry: './src/app.js',
+      output: {
+         path: path.join(__dirname, 'public'),
+         filename: 'bundle.js'
       },
-      {
-         test: /\.s?css$/,
-         use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-         ]
+      mode: 'development',
+      module: {
+         rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+         },
+         {
+            test: /\.s?css$/,
+            use: [
+               MiniExtractPlugin.loader,
+               {
+                  loader: 'css-loader',
+                  options: {
+                     sourceMap: true
+                  }
+               },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     sourceMap: true
+                  }
+               }
+            ]
+         }]
+      },
+      plugins: [
+         CSSExtract
+      ],
+      devtool: isProduction ? 'source-map' : 'inline-source-map',
+      devServer: {
+         contentBase: path.join(__dirname, 'public'),
+         historyApiFallback: true
       }
-   ]
-   },
-   devtool: 'cheap-module-eval-source-map',
-   devServer: {
-      contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
    }
 };
